d6def094bcb20a93ef677128a90288f824727041
shahzaib887 <shahzaibshazzo@gmail.com>
2026-02-12 13:54:01 +0500

    remove unused auth and register pages; clean up layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SidebarProvider } from "@/context/SidebarContext";
-import { Sidebar } from "@/components/Sidebar";
-import { Navbar } from "@/components/Navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Splitwise",
-  description: "Manage expenses",
-};
 
 export default function RootLayout({
   children,
@@ -27,10 +9,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        cz-shortcut-listen="true"
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
-      >
+      <body cz-shortcut-listen="true" className={` antialiased bg-black`}>
         <main className="">{children}</main>
       </body>
     </html>
