51a9a6bd754fe30cc0d561b648a9fbbcb135e5d1
shahzaib887 <shahzaibshazzo@gmail.com>
2026-02-15 13:23:07 +0500

    add create riute

---
+++ src/app/api/(dashboard)/group/create/route.ts
@@ -0,0 +1,80 @@
+import { NextRequest, NextResponse } from "next/server";
+import { ConnectDB } from "@/lib/ConnectDB";
+import { Group } from "@/models/group.model";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+export async function POST(request: NextRequest) {
+  try {
+    await ConnectDB();
+
+    const body = await request.json();
+    const { name, totalAmount } = body;
+    const cookieStore = await cookies(); // ✅ important
+    const token = cookieStore.get("token")?.value;
+    if (!token) {
+      return NextResponse.json(
+        { success: false, message: "Authentication token required" },
+        { status: 401 },
+      );
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET!);
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    console.log(decoded);
+
+    if (name === undefined || totalAmount === undefined) {
+      return NextResponse.json(
+        { success: false, message: "Name and totalAmount are required" },
+        { status: 400 },
+      );
+    }
+
+    if (typeof name !== "string" || name.trim().length === 0) {
+      return NextResponse.json(
+        { success: false, message: "Name must be a valid non-empty string" },
+        { status: 400 },
+      );
+    }
+
+    const parsedAmount = Number(totalAmount);
+
+    if (isNaN(parsedAmount) || parsedAmount <= 0) {
+      return NextResponse.json(
+        { success: false, message: "Total amount must be a positive number" },
+        { status: 400 },
+      );
+    }
+
+    const existingGroup = await Group.findOne({ name: name.trim() });
+
+    if (existingGroup) {
+      return NextResponse.json(
+        { success: false, message: "Group already exists" },
+        { status: 409 },
+      );
+    }
+
+    const newGroup = await Group.create({
+      name: name.trim().toString(),
+      totalAmount: parsedAmount,
+      createdBy: decode.userId,
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Group created successfully",
+        data: newGroup,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Create Group Error:", error);
+
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
--- src/models/group.model.tsx
+++ src/models/group.model.tsx
@@ -2,6 +2,7 @@ import mongoose, { Schema, Document, Model } from "mongoose";
 
 export interface IGroup extends Document {
   name: string;
+  totalAmount: number;
   createdBy: mongoose.Types.ObjectId;
   createdAt: Date;
   updatedAt: Date;
@@ -10,10 +11,11 @@ export interface IGroup extends Document {
 const GroupSchema = new Schema<IGroup>(
   {
     name: { type: String, required: true },
+    totalAmount: { type: Number, required: true },
     createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-export const Group: Model<IGroup> =
+export const Group : Model<IGroup> =
   mongoose.models.Group || mongoose.model<IGroup>("Group", GroupSchema);
