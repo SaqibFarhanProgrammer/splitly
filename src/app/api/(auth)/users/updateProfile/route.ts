e28c39c940402c79e2998d46c2896817764f57d5
SaqibFarhanProgrammer <saqibkhatai@gmail.com>
2026-06-29 16:36:27 +0500

    fix

--- src/app/api/(auth)/users/updateProfile/route.ts
+++ src/app/api/(auth)/users/updateProfile/route.ts
@@ -1,6 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { jwtVerify } from 'jose';
-import { cookies } from 'next/headers';
 import { User } from '@/models/user.model';
 import { getUserIdFromToken } from '@/lib/GetToken';
 
